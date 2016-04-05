82e6aae204e4f764679159590283c94230eb2a2f
Dzheylyan Ademova <djeni.ademova@gmail.com>
2016-04-05 14:43:16 +0300

    Accurately renamed projects

---
+++ VegiJ.DataAccess/Mapping/UserMap.cs
@@ -0,0 +1,23 @@
+﻿namespace VegiJ.DataAccess.Mapping
+{
+    using System.Data.Entity.ModelConfiguration;
+
+    public class UserMap : EntityTypeConfiguration<User>
+    {
+        public UserMap()
+        {
+            //key
+            HasKey(t => t.ID);
+            // properties
+            Property(t => t.UserName);
+            Property(t => t.Password);
+            Property(t => t.Email);
+            Property(t => t.CreatedDate);
+            Property(t => t.IsAdmin);
+            Property(t => t.LastLoginDate);
+            Property(t => t.ModifiedDate);
+            // table
+            ToTable("Users"); // primerno
+        }
+    }
+}
