ab4930fb9cbd8e3aa7fb012b001ce1798b34d824
Dzheylyan Ademova <djeni.ademova@gmail.com>
2016-04-15 17:39:06 +0300

    Implemented Category class

---
+++ VegiJ.DataAccess/Category.cs
@@ -0,0 +1,15 @@
+﻿namespace VegiJ.DataAccess
+{
+    using System;
+    using System.Collections.Generic;
+    using System.ComponentModel.DataAnnotations;
+    public class Category
+    {
+        [Key]
+        public Guid Id { get; private set; }
+        public string Name { get; private set; }
+        public Guid? ParentCategoryId { get; private set; }
+        public virtual Category ParentCategory { get; private set; }
+        public virtual ICollection<Category> SubCategories { get; private set; }
+    }
+}
